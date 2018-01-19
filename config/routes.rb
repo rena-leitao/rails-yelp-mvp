5d8be30df96103b4d975051ced6607f0bcd40a65
Renata Leitao <asadebarata@gmail.com>
2018-01-19 17:02:09 -0200

    trabalhando na router e controller de review

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+    get 'reviews/new'
+
+    get 'reviews/create'
+
+   resources :restaurants, except: [:edit, :update, :destroy] do
+    resources :reviews, only: [ :index, :new, :create ]
+    end
+  resources :reviews, only: [ :show, :edit, :update, :destroy ]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Mulan',
+    address:      'savassi',
+    phone_number:  '31 999 1234',
+    category:      'chinese'
+  },
+  {
+    name:         'unaltra volta',
+    address:      'sion',
+    phone_number:  '19 27 34 56',
+    category:      'italian'
+  }
+    {
+    name:         'sushi naka',
+    address:      'funcionarios',
+    phone_number:  '123 456 789',
+    category:       'japanese'
+  },
+  {
+    name:         'Taste vin',
+    address:      'lourdes',
+    phone_number:  '123 456 987',
+    category:      'french'
+  }
+    {
+    name:         'Stadt jever',
+    address:      'contorno',
+    phone_number:  '666 696 8594',
+    category:        'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
